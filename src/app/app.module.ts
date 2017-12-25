d2c9cb41bf6e5cd2ae8393b5fd6c9b1ada665f02
aniket umbc <aniket.umbc@gmail.com>
2017-12-25 17:17:09 +0530

    Component Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
